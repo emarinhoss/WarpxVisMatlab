e72b93105ca20f9a16d783278af38d94aece60cf
Eder Sousa <sousae@uw.edu>
2011-06-14 14:21:43 -0700

    make 2d videos ability added

--- wx2dvis.py
+++ wx2dvis.py
@@ -1,13 +1,15 @@
 import os
+import subprocess
 import wxdata as wxdata2
 from pylab import *
 from numpy import *
 
+
 frames = 40
-filename = "/home/sousae/dbg_warpx/recon_mmc/ssrecon_wv"
+location = "/home/sousae/dbg_warpx/recon_mmc/ssrecon_wv"
 
 for n in range(0,frames+1):
-		dh = wxdata2.WxData(filename, n)
+		dh = wxdata2.WxData(location, n)
 		q = dh.read('qnew')
 		x = linspace(q.grid.lowerBounds[0],q.grid.upperBounds[0],q.grid.numPhysCells[0])
 		y = linspace(q.grid.lowerBounds[1],q.grid.upperBounds[1],q.grid.numPhysCells[1])
@@ -15,5 +17,24 @@ for n in range(0,frames+1):
 		by = q[:, :, 0]
 		
 		pcolor(Y,X,by)
+		filename = str('%03d' % n) + '_recon.png'
+		savefig(filename, dpi=100)
+		clf()
 		
-		draw()
+		dh.close()
+
+
+command = ('mencoder',
+           'mf://*_recon.png',
+           '-mf',
+           'type=png:w=800:h=600:fps=24',
+           '-ovc',
+           'lavc',
+           '-lavcopts',
+           'vcodec=mpeg4',
+           '-oac',
+           'copy',
+           '-o',
+           'output.avi')
+           
+subprocess.check_call(command)           
